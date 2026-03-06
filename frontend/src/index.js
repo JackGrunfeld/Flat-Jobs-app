3c6555ba325afe28c1fe1289056c1dd8127fbba0
Jack Grunfeld <grunfejack@ecs.vuw.ac.nz>
2026-03-06 15:58:17 +1300

    Adding logic for service

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app';
 // reportWebVitals is optional; create the file or remove this import if not using it
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
