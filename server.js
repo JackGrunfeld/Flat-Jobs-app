999456f51ac154c242c242ef590832bc92ed4b6d
Jack Grunfeld <grunfejack@ecs.vuw.ac.nz>
2026-03-06 16:13:53 +1300

    Adding logic for service

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.post("/history", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`\n🚀 Server running on port ${PORT}`));
